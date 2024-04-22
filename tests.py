1640ac33752e20418f1cdb5eae54b5e6574c3cc6
Sergey Kugushev <kugu88@ya.ru>
2024-04-22 11:15:25 +0300

    Sprint_4

--- tests.py
+++ tests.py
@@ -1,24 +1,75 @@
-from main import BooksCollector
-
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+import pytest
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book_adding_a_new_book(self, books_collector):
+        books_collector.add_new_book("Красная шапочка")
+        assert "Красная шапочка" in books_collector.get_books_genre()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_invalid_name(self,books_collector):
+        books_collector.add_new_book("Преступление и наказание" * 20)
+        assert "Преступление и наказание" * 20 not in books_collector.get_books_genre()
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    @pytest.mark.parametrize("genre", ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
+    def test_set_book_genre_assigning_genre(self,books_collector, genre):
+        books_collector.add_new_book("Молчание ягнят")
+        books_collector.set_book_genre("Молчание ягнят", genre)
+        assert books_collector.get_book_genre("Молчание ягнят") == genre
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_genre_invalid_book(self, books_collector):
+        books_collector.set_book_genre("Властелин колец", "Фантастика")
+        assert books_collector.get_book_genre("Властелин колец") is None or books_collector.get_book_genre("Властелин колец") == ""
+
+    def test_get_book_genre_getting_genre(self,books_collector):
+        books_collector.add_new_book("Оно")
+        books_collector.set_book_genre("Оно", "Ужасы")
+        assert books_collector.get_book_genre("Оно") == "Ужасы"
+
+    def test_get_book_genre_invalid_name(self,books_collector):
+        assert books_collector.get_book_genre("Код да Винчи") is None
+
+    def test_get_books_with_specific_genre_list_specific_genre(self,books_collector):
+        books_collector.add_new_book("Убийство на улице Морг")
+        books_collector.set_book_genre("Убийство на улице Морг", "Детективы")
+        assert "Убийство на улице Морг" in books_collector.get_books_with_specific_genre("Детективы")
+
+    def test_get_books_with_specific_genre_invalid_genre(self,books_collector):
+        assert books_collector.get_books_with_specific_genre("Романтика") == []
+
+    def test_get_books_for_children_children_books(self,books_collector):
+        books_collector.add_new_book("Простоквашино")
+        books_collector.set_book_genre("Простоквашино", "Мультфильмы")
+        assert "Простоквашино" in books_collector.get_books_for_children()
+
+    def test_get_books_for_children_invalid_genre(self,books_collector):
+        books_collector.add_new_book("Вий")
+        books_collector.set_book_genre("Вий", "Ужасы")
+        assert "Вий" not in books_collector.get_books_for_children()
+
+    def test_add_book_in_favorites_adding_book(self,books_collector):
+        books_collector.add_new_book("Дюна")
+        books_collector.add_book_in_favorites("Дюна")
+        assert "Дюна" in books_collector.get_list_of_favorites_books()
+
+    def test_add_book_in_favorites_already_added(self,books_collector):
+        books_collector.add_new_book("Повелитель мух")
+        books_collector.add_book_in_favorites("Повелитель мух")
+        books_collector.add_book_in_favorites("Повелитель мух")
+        assert books_collector.get_list_of_favorites_books().count("Повелитель мух") == 1
+
+    def test_delete_book_from_favorites(self,books_collector):
+        books_collector.add_new_book("Вторая жизнь Уве")
+        books_collector.add_book_in_favorites("Вторая жизнь Уве")
+        books_collector.delete_book_from_favorites("Вторая жизнь Уве")
+        assert "Вторая жизнь Уве" not in books_collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites_not_in_favorites(self,books_collector):
+        books_collector.delete_book_from_favorites("Мастер и Маргарита")
+        assert books_collector.get_list_of_favorites_books() == []
+
+    def test_get_list_of_favorites_books(self,books_collector):
+        books_collector.add_new_book("Важные годы")
+        books_collector.add_book_in_favorites("Важные годы")
+        assert books_collector.get_list_of_favorites_books() == ["Важные годы"]
+
+    def test_get_list_of_favorites_books_empty(self,books_collector):
+        assert books_collector.get_list_of_favorites_books() == []
